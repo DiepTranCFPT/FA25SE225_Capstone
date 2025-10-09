e6277740429555b53eb2653dd572533ba8820fe9
Diep Tran <trancaodiepnct@gmail.com>
2025-10-09 15:12:48 +0700

    fix(API): /outbound/authentication [Post] title : authorize GG create By : Diep

--- src/main/java/com/fa25se225/capstone/configuration/SecurityConfig.java
+++ src/main/java/com/fa25se225/capstone/configuration/SecurityConfig.java
@@ -28,7 +28,7 @@ import org.springframework.web.cors.CorsConfiguration;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final String[] PUBLIC_ENDPOINTS_POST = {"/users", "auth/token", "auth/introspect", "auth/logout", "auth/refresh-token", "auth/reset-password/**", "test/**","/auth/verify-email"};
+    private final String[] PUBLIC_ENDPOINTS_POST = {"/users", "auth/token", "auth/introspect", "auth/logout", "auth/refresh-token", "auth/reset-password/**", "test/**","/auth/verify-email","/auth/outbound/authentication"};
     private final String[] PUBLIC_ENDPOINTS_GET = {"auth/forgot-password", "test/**", "products/search", "products", "products/list",};
     private static final String[] SWAGGER_UI_PATHS = {
             "/v3/api-docs/**",

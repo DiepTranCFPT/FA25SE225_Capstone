3c16ae3c271b4023e6551ed79f9f224afec9988e
iehuhamp <hieuphamtrung2910@gmail.com>
2025-10-29 18:13:30 +0700

    update config for question difficulty mapper

--- src/main/java/com/fa25se225/capstone/mapper/QuestionDifficultyMapper.java
+++ src/main/java/com/fa25se225/capstone/mapper/QuestionDifficultyMapper.java
@@ -7,8 +7,11 @@ import com.fa25se225.capstone.entity.QuestionDifficulty;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring", 
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface QuestionDifficultyMapper {
     
     QuestionDifficulty toEntity(QuestionDifficultyCreationRequest request);

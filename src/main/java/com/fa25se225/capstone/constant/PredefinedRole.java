e33423a000d257582219b33396ab5618584a5909
Diep Tran <trancaodiepnct@gmail.com>
2025-10-31 15:19:47 +0700

    title : Update create By : Diep

--- src/main/java/com/fa25se225/capstone/constant/PredefinedRole.java
+++ src/main/java/com/fa25se225/capstone/constant/PredefinedRole.java
@@ -3,6 +3,9 @@ package com.fa25se225.capstone.constant;
 public class PredefinedRole {
     public static final String USER_ROLE = "USER";
     public static final String ADMIN_ROLE = "ADMIN";
+    public static final String TEACHER_ROLE = "TEACHER";
+
+
 
     private PredefinedRole() {}
 }

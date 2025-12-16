c035a62eeb21cf49430264bdfe5e2c88c071506e
tridovan <tridvse173191@fpt.edu.vn>
2025-12-16 17:14:45 +0700

    feat(flashcard): implement flashcard

---
+++ src/main/java/com/fa25se225/capstone/dto/response/FlashcardSetDetailResponse.java
@@ -0,0 +1,23 @@
+package com.fa25se225.capstone.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Data
+@Builder
+public class FlashcardSetDetailResponse {
+    private String id;
+    private String title;
+    private String description;
+    private boolean isPublic;
+    private int cardCount;
+    private int viewCount;
+    private UserResponse author;
+    private LocalDateTime createdAt;
+    private List<FlashcardResponse> flashcards;
+}
+

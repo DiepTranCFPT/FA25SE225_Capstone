d04b4392769d0cbdc739ccb4ff4ed9e7df78a035
Diep Tran <trancaodiepnct@gmail.com>
2025-12-24 14:41:30 +0700

    title : fix websocket create By : Diep

--- src/main/java/com/fa25se225/capstone/configuration/WebSocketConfig.java
+++ src/main/java/com/fa25se225/capstone/configuration/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.fa25se225.capstone.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -20,7 +19,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns("http://localhost:5173",
+                        "https://www.ap-learning.online");
     }
 }
 

1ef98190113dd4b79e2545751f936ae1e624ee9a
Diep Tran <trancaodiepnct@gmail.com>
2025-12-26 18:52:46 +0700

    title : update get request teacher verify create By : Diep

--- src/main/java/com/fa25se225/capstone/service/implementation/UserServiceImpl.java
+++ src/main/java/com/fa25se225/capstone/service/implementation/UserServiceImpl.java
@@ -228,10 +228,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-/*    @Caching(evict = {
-            @CacheEvict(value = "teacher_profile", key = "#userId"),
-            @CacheEvict(value = "userId", key = "#userId")
-    })*/
     public UserResponse verifyTeacher(String userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
